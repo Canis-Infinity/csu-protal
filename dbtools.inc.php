efab066c9463dec1ad2dac80144d603ad8500070
張永昌 <josh22788@gmail.com>
2021-05-21 19:19:34 +0800

    Update dbtools.inc.php

--- dbtools.inc.php
+++ dbtools.inc.php
@@ -1,6 +1,6 @@
 <?php
     function create_connection() {
-        $link = mysqli_connect("localhost","root","canis1227", "csu_portal") or die("無法連接資料庫：".mysqli_connect_error());
+        $link = mysqli_connect("localhost","root","******", "csu_portal") or die("無法連接資料庫：".mysqli_connect_error());
         mysqli_query($link, "SET NAMES utf8");
         return $link;
     }
